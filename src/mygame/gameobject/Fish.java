f4c8e65d181fb29a32ddb281e67210a8eb82ee0c
HubbyMon <ariana.hou@gmail.com>
2021-01-13 12:52:34 -0500

    fish class

--- src/mygame/gameobject/Fish.java
+++ src/mygame/gameobject/Fish.java
@@ -3,12 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package mygame.gameobject;
+
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.BoxCollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue;
+import mygame.state.Main;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bounding.BoundingBox;
 
 /**
  *
  * @author ariana
  */
-public class Fish {
+public class Fish extends Enemy {
     
 }
